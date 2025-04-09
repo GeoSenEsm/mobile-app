9b387d79ef2c6d30f1fa521c2eb3068c69a16a63
Pawel Felcyn <pawel.felcyn@onet.pl>
2025-04-09 19:53:56 +0200

    localize provacy policy

--- lib/presentation/screens/privacy_policy/widgets/privacy_policy_content.dart
+++ lib/presentation/screens/privacy_policy/widgets/privacy_policy_content.dart
@@ -4,6 +4,7 @@ import 'package:flutter_html/flutter_html.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:survey_frontend/presentation/static/static_variables.dart';
 
 
 class PrivacyPolicyContent extends StatefulWidget{
@@ -48,15 +49,23 @@ class _PrivacyPolicyContentState extends State<PrivacyPolicyContent>{
             );
   }
 
-    void fetchHtmlContent() async {
+  void fetchHtmlContent() async {
+   await _loadPrivacyPolicy('/privacy-policy/${StaticVariables.lang}.html');
+  }
+
+  Future<void> _loadPrivacyPolicy(String url) async {
     try {
       final apiUrl = await GetStorage().read('apiUrl');
       final response = await http.get(
-          Uri.parse(apiUrl + '/privacy-policy/pl.html'));
+          Uri.parse(apiUrl + url));
       if (response.statusCode == 200) {
         setState(() {
           htmlContent = utf8.decode(response.bodyBytes);
         });
+      }
+
+      if (response.statusCode == 404 && url != '/privacy-policy/en.html') {
+        await _loadPrivacyPolicy('/privacy-policy/en.html');
       }
     } catch (e) {
       Sentry.captureException(e);
